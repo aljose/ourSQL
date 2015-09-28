2cea1f958f2886e1c9c9d716b55ba8860961b42e
majesco <nicolas.j2007@gmail.com>
2015-09-28 15:10:15 -0600

    drop table commit

--- ourSQL/src/DatabaseRuntimeProcessor/DropTable.java
+++ ourSQL/src/DatabaseRuntimeProcessor/DropTable.java
@@ -1,120 +1,111 @@
 package DatabaseRuntimeProcessor;
 
-import Shared.Structures.Field;
 import Shared.Structures.Metadata;
-import Shared.Structures.Row;
-import Shared.Structures.Table;
+
 import StoredDataManager.Main.StoredDataManager;
 import SystemCatalog.Constants;
 import java.util.ArrayList;
 
-
-
- 
- 
-
- /*
+/*
  *@author Nicolas Jimenez
  *
-*/
+ */
 public class DropTable {
 
- 
-/* 
-* @param nombreEsquema
- *@param nombreTabla
- */
- public void dropTable(String nombreEsquema, String nombreTabla) {
+    /* 
+     * @param nombreEsquema
+     *@param nombreTabla
+     */
+    public void dropTable(String nombreEsquema, String nombreTabla) {
 
- if (!verifyIntegrity(nombreEsquema, nombreTabla)) {
- return;
- }
+        if (!verifyIntegrity(nombreEsquema, nombreTabla)) {
+            return;
+        }
 
- deleteFromMetadata(nombreEsquema, nombreTabla);
- deleteFromDisc(nombreEsquema, nombreTabla);
- }
+        deleteFromMetadata(nombreEsquema, nombreTabla);
+        deleteFromDisc(nombreEsquema, nombreTabla);
+    }
 
- 
-/* 
- *@param nombreEsquema
- *@param nombreTabla
- *@return
- */
- public boolean verifyIntegrity(String nombreEsquema, String nombreTabla) {
+    /* 
+     *@param nombreEsquema
+     *@param nombreTabla
+     *@return
+     */
+    public boolean verifyIntegrity(String nombreEsquema, String nombreTabla) {
 
- StoredDataManager storer =  StoredDataManager.getInstance();
- Metadata meta = storer.deserealizateMetadata();
+        StoredDataManager storer = StoredDataManager.getInstance();
+        Metadata meta = storer.deserealizateMetadata();
 
- ArrayList<ArrayList<String>> tablaTabla = meta.getMetadata().get(Constants.TABLES);
+        ArrayList<ArrayList<String>> tablaTabla = meta.getMetadata().get(Constants.TABLES);
 
- for (int i = 0; i< tablaTabla.size(); i++) {
+        for (int i = 0; i < tablaTabla.size(); i++) {
 
- ArrayList<String> fila = tablaTabla.get(i);
+            ArrayList<String> fila = tablaTabla.get(i);
 
- if (fila.get(Constants.TABLE_FK).equals(true)) {
+            if (fila.get(Constants.TABLE_FK).equals(true)) {
 
- return false;
- }
- }
- return true;
- }
+                return false;
+            }
+        }
+        return true;
+    }
 
- 
- 
- /*@param databaseName
- *@param nombreEsquema
- *@param nombreTabla
- */
- public void deleteFromMetadata(String databaseName, String nombreTabla) {
+    /*@param databaseName
+     *@param nombreEsquema
+     *@param nombreTabla
+     */
+    public void deleteFromMetadata(String databaseName, String nombreTabla) {
 
- StoredDataManager storer = StoredDataManager.getInstance();
- Metadata meta = storer.deserealizateMetadata();
+        StoredDataManager storer = StoredDataManager.getInstance();
+        Metadata meta = storer.deserealizateMetadata();
+        
+        storer.initStoredDataManager(databaseName);
 
- ArrayList<ArrayList<String>> tablaTabla = meta.getMetadata().get(Constants.TABLES);
+        ArrayList<ArrayList<String>> tablaTabla = meta.getMetadata().get(Constants.TABLES);
 
- for (int i = 0; i< tablaTabla.size(); i++) {
+        for (int i = 0; i < tablaTabla.size(); i++) {
 
- ArrayList<String> fila = tablaTabla.get(i);
+            ArrayList<String> fila = tablaTabla.get(i);
 
- if (fila.get(Constants.TABLE_SCHNAME).equals(databaseName)) {
+            if (fila.get(Constants.TABLE_SCHNAME).equals(databaseName)) {
 
- meta.getMetadata().get(Constants.TABLES).remove(i);
- }
- }
- ArrayList<ArrayList<String>> tablaCol = meta.getMetadata().get(Constants.COLUMNS);
+                meta.getMetadata().get(Constants.TABLES).remove(i);
+            }
+        }
+        ArrayList<ArrayList<String>> tablaCol = meta.getMetadata().get(Constants.COLUMNS);
 
- for (int i = 0; i <tablaCol.size(); i++) {
+        for (int i = 0; i < tablaCol.size(); i++) {
 
- ArrayList<String> fila = tablaCol.get(i);
+            ArrayList<String> fila = tablaCol.get(i);
 
- if (fila.get(Constants.COLUMNS_SCHNAME).equals(databaseName)
- && fila.get(Constants.COLUMNS_TABNAME).equals(nombreTabla)) {
+            if (fila.get(Constants.COLUMNS_SCHNAME).equals(databaseName)
+                    && fila.get(Constants.COLUMNS_TABNAME).equals(nombreTabla)) {
 
- meta.getMetadata().get(Constants.COLUMNS).remove(i);
- }
- }
+                meta.getMetadata().get(Constants.COLUMNS).remove(i);
+            }
+        }
 
- ArrayList<ArrayList<String>> tablaFor = meta.getMetadata().get(Constants.FOREIGNKEY);
+        ArrayList<ArrayList<String>> tablaFor = meta.getMetadata().get(Constants.FOREIGNKEY);
 
- for (int i = 0; i< tablaFor.size(); i++) {
+        for (int i = 0; i < tablaFor.size(); i++) {
 
- ArrayList<String> fila = tablaFor.get(i);
+            ArrayList<String> fila = tablaFor.get(i);
 
- if (fila.get(Constants.FK_SCHNAME).equals(databaseName)) {
+            if (fila.get(Constants.FK_SCHNAME).equals(databaseName)) {
 
- meta.getMetadata().get(Constants.FOREIGNKEY).remove(i);
- }
- }
- ArrayList<ArrayList<ArrayList<String>>> metadata = meta.getMetadata();// variable donder se guarda al final
- meta.setMetadata(metadata);
- storer.serializeMetadata(meta);
- }
+                meta.getMetadata().get(Constants.FOREIGNKEY).remove(i);
+            }
+        }
+        ArrayList<ArrayList<ArrayList<String>>> metadata = meta.getMetadata();// variable donder se guarda al final
+        meta.setMetadata(metadata);
+        storer.serializeMetadata(meta);
+    }
 
- public void deleteFromDisc(String databaseName, String tableName) {
+    public void deleteFromDisc(String databaseName, String tableName) {
 
- StoredDataManager temp =  StoredDataManager.getInstance();
- temp.initStoredDataManager(databaseName);
- System.out.println("Drop table " + temp.dropTable(tableName));
- }
+        StoredDataManager temp = StoredDataManager.getInstance();
+        temp.initStoredDataManager(databaseName);
+        System.out.println("Drop table " + temp.dropTable(tableName));
+    }
 
 }
